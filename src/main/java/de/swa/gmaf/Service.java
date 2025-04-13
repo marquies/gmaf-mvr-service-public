98ad246335a796f9e829d8e6b688738883f26e34
Patrick Steinert <me@patrickbreucking.de>
2025-04-13 15:58:49 +0200

    Changes for swagger and openapi

--- src/main/java/de/swa/gmaf/Service.java
+++ src/main/java/de/swa/gmaf/Service.java
@@ -1,34 +1,30 @@
 package de.swa.gmaf;
 
-import java.io.IOException;
 import java.net.URI;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
 import javax.xml.ws.Endpoint;
 
-import com.sun.net.httpserver.HttpServer;
-import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
-import de.swa.gmaf.api.GMAF_Facade_RESTImpl;
 import de.swa.gmaf.api.GMAF_Facade_SOAPImpl;
 import de.swa.ui.AutoProcessThread;
 import de.swa.ui.Configuration;
 
 public class Service {
-    private static HttpServer restServer;
-    private static Endpoint soapEndpoint;
-    private volatile boolean running = false;
+	private static HttpServer restServer;
+	private static Endpoint soapEndpoint;
+	private volatile boolean running = false;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		new Service().start();
 	}
 
 	public synchronized void start() {
-        if (running) {
-            return;
-        }
+		if (running) {
+			return;
+		}
 		// if auto processing is activated, check for MMFGs
 		if (Configuration.getInstance().isAutoProcess()) {
 			AutoProcessThread at = new AutoProcessThread();
@@ -36,62 +32,60 @@ public class Service {
 			at.start();
 		}
 
+
 		String api = "http://" + Configuration.getInstance().getServerName() + ":"
 				+ Configuration.getInstance().getServerPort() + "/" + Configuration.getInstance().getContext()
 				+ "/gmafApi";
 
-		String restApi = "http://" + Configuration.getInstance().getServerName() + ":"
-				+ Configuration.getInstance().getRestServicePort() + "/" + Configuration.getInstance().getContext()
+		String url = "http://" + Configuration.getInstance().getServerName() + ":"
+				+ Configuration.getInstance().getRestServicePort();
+		String restApi = url + "/" + Configuration.getInstance().getContext()
 				+ "/gmafApi";
 
-		GMAF_Facade_RESTImpl resourceConfig = new GMAF_Facade_RESTImpl();
-		resourceConfig.register(new CORSFilter());
-		restServer = JdkHttpServerFactory.createHttpServer(URI.create(restApi), resourceConfig);
+		Api apiConfig = new Api();
+		apiConfig.register(new CORSFilter());
+
+		restServer = GrizzlyHttpServerFactory.createHttpServer(
+				URI.create(restApi),
+				apiConfig
+		);
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		CLStaticHttpHandler swaggerUiHandler = new CLStaticHttpHandler(cl, "/META-INF/resources/webjars/swagger-ui/5.20.7/");
+		restServer.getServerConfiguration().addHttpHandler(swaggerUiHandler, "/swagger-ui");
 
 		System.out.println("GMAF SOAP API@: " + api);
 		System.out.println("GMAF REST API@: " + restApi);
+		System.out.println("Swagger UI available at: " + url + "/swagger-ui/index.html and the OpenAPI at: " + url + "/gmaf/gmafApi/openapi.json");
+		
 		soapEndpoint = Endpoint.publish(api, new GMAF_Facade_SOAPImpl());
-        // Start auto processing if enabled
+		// Start auto processing if enabled
 
-        running = true;
+		running = true;
 		System.out.println("GMAF Service running for collection: " + Configuration.getInstance().getCollectionName());
 	}
 
-    public synchronized void stop() {
-        if (!running) {
-            return;
-        }
+	public synchronized void stop() {
+		if (!running) {
+			return;
+		}
 
-        try {
-            if (restServer != null) {
-                restServer.stop(0);
-                restServer = null;
-            }
+		try {
+			if (restServer != null) {
+				restServer.stop();
+				restServer = null;
+			}
 
-            if (soapEndpoint != null) {
-                soapEndpoint.stop();
-                soapEndpoint = null;
-            }
+			if (soapEndpoint != null) {
+				soapEndpoint.stop();
+				soapEndpoint = null;
+			}
 
 
-
-            running = false;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static class CORSFilter implements ContainerResponseFilter {
-		@Override
-		public void filter(ContainerRequestContext request,
-						   ContainerResponseContext response) throws IOException {
-			response.getHeaders().add("Access-Control-Allow-Origin", "*");
-			response.getHeaders().add("Access-Control-Allow-Headers",
-					"CSRF-Token, X-Requested-By, Authorization, Content-Type");
-			response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-			response.getHeaders().add("Access-Control-Allow-Methods",
-					"GET, POST, PUT, DELETE, OPTIONS, HEAD");
+			running = false;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
+
 }
 	
